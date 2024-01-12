fc28023e01761f11e0d9d4e3f5c2256f0d86e9ab
samuelea <batonenbois@gmail.com>
2024-01-12 13:22:25 -0500

    wagmi v2 update

--- src/sequence-connector.ts
+++ src/sequence-connector.ts
@@ -8,142 +8,182 @@ import {
 
 import {
   Connector,
-  ConnectorData,
-  Chain,
+  createConnector
 } from 'wagmi'
 
-export interface Options {
+export interface SequenceParameters {
   defaultNetwork?: sequence.network.ChainIdLike,
   connect?: sequence.provider.ConnectOptions & { walletAppURL?: string }
 }
 
-export class SequenceConnector extends Connector<sequence.provider.SequenceProvider, Options | undefined> {
-  id = 'sequence'
-  name = 'Sequence'
+sequenceWallet.type = 'sequence' as const
 
-  ready = true
+export function sequenceWallet(params: SequenceParameters) {
+  const {  
+    defaultNetwork,
+    connect
+  } = params
 
-  provider: sequence.provider.SequenceProvider
+  const id = 'sequence'
+  const name = 'Sequence'
 
-  constructor({ chains, options }: {
-    chains?: Chain[],
-    options?: Options
-  }) {
-    super({ chains, options })
 
-    this.provider = sequence.initWallet({
-      defaultNetwork: options?.defaultNetwork,
-      transports: {
-        walletAppURL: options?.connect.walletAppURL || 'https://sequence.app',
-      },
-      defaultEIP6492: true,
-      projectAccessKey: options?.connect?.projectAccessKey
-    })
+  let provider: sequence.provider.SequenceProvider
+  type Provider = sequence.provider.SequenceProvider
+  type Properties = {}
 
-    this.provider.on('chainChanged', (chainIdHex: string) => {
-      // @ts-ignore-next-line
-      this?.emit('change', { chain: { id: normalizeChainId(chainIdHex), unsupported: false } })
-    })
+  provider = sequence.initWallet({
+    defaultNetwork: defaultNetwork,
+    transports: {
+      walletAppURL: connect.walletAppURL || 'https://sequence.app',
+    },
+    defaultEIP6492: true,
+    projectAccessKey: connect?.projectAccessKey
+  })
 
-    this.provider.on('accountsChanged', (accounts: string[]) => {
-      // @ts-ignore-next-line
-      this?.emit('accountsChanged', this.onAccountsChanged(accounts))
-    })
+  return createConnector<Provider, Properties>(config => ({
+    id: 'sequence',
+    name: 'Sequence',
+    type: sequenceWallet.type,
+    async setup() {
+      // provider.on('chainChanged', (chainIdHex: string) => {
+      //   // @ts-ignore-next-line
+      //   config.emitter.emit('change', { chain: { id: normalizeChainId(chainIdHex), unsupported: false } })
+      // })
 
-    this.provider.on('disconnect', () => {
-      this.onDisconnect()
-    })
-  }
+      // provider.on('accountsChanged', (accounts: string[]) => {
+      //   // @ts-ignore-next-line
+      //   config.emitter.emit('accountsChanged', this.onAccountsChanged(accounts))
+      // })
 
-  async connect(): Promise<Required<ConnectorData>> {
-    if (!this.provider.isConnected()) {
-      // @ts-ignore-next-line
-      this?.emit('message', { type: 'connecting' })
-      const e = await this.provider.connect(this.options?.connect ?? { app: 'app' })
-      if (e.error) {
-        throw new UserRejectedRequestError(new Error(e.error))
-      }
-      if (!e.connected) {
-        throw new UserRejectedRequestError(new Error('Wallet connection rejected'))
-      }
+      // provider.on('disconnect', () => {
+      //   onDisconnect()
+      // })
+    },
+    async connect({ chainId, isReconnecting } = {}) {
+      return ({
+        accounts: [],
+        chainId
+      })
+    },
+    async disconnect() {
+
+    },
+    async getAccounts() {
+      return []
+    },
+    async getProvider() {
+      return provider
+    },
+    async isAuthorized() {
+      return true
+    },
+    async switchChain({ chainId }) {
+      return config.chains[0]
+    },
+    async onAccountsChanged(accounts) {
+
+    },
+    async onChainChanged(chain) {
+
+    },
+    async onConnect(connectinfo) {
+
+    },
+    async onDisconnect() {
+
     }
-
-    const account = await this.getAccount()
-
-    return {
-      account,
-      chain: {
-        id: this.provider.getChainId(),
-        unsupported: this.isChainUnsupported(this.provider.getChainId()),
-      },
-    }
-  }
-
-  async getWalletClient({ chainId }: { chainId?: number } = {}): Promise<any> {
-    const chain = this.chains.find((x) => x.id === chainId)
-
-    return createWalletClient({
-      chain,
-      account: await this.getAccount(),
-      transport: custom(this.provider),
-    })
-  }
-
-  protected onChainChanged(chain: string | number): void {
-    this.provider.setDefaultChainId(normalizeChainId(chain))
-  }
-
-  async switchChain(chainId: number): Promise<Chain> {
-    if (this.isChainUnsupported(chainId)) {
-      throw new Error('Unsupported chain')
-    }
-
-    this.provider.setDefaultChainId(chainId)
-    return this.chains.find((x) => x.id === chainId) as Chain
-  }
-
-  async disconnect() {
-    this.provider.disconnect()
-  }
-
-  getAccount() {
-    return this.provider.getSigner().getAddress() as Promise<`0x${string}`>
-  }
-
-  async getChainId() {
-    return this.provider.getChainId()
-  }
-
-  async getProvider() {
-    return this.provider
-  }
-
-  async getSigner() {
-    return this.provider.getSigner()
-  }
-
-  async isAuthorized() {
-    try {
-      const account = await this.getAccount()
-      return !!account
-    } catch {
-      return false
-    }
-  }
-
-  protected onAccountsChanged = (accounts: string[]) => {
-    return { account: accounts[0] }
-  }
-
-  protected onDisconnect = () => {
-    // @ts-ignore-next-line
-    this?.emit('disconnect')
-  }
-
-  isChainUnsupported(chainId: number): boolean {
-    return this.provider.networks.findIndex((x) => x.chainId === chainId) === -1
-  }
+  }))
 }
+
+  // async connect(): Promise<Required<ConnectorData>> {
+  //   if (!this.provider.isConnected()) {
+  //     // @ts-ignore-next-line
+  //     this?.emit('message', { type: 'connecting' })
+  //     const e = await this.provider.connect(this.options?.connect ?? { app: 'app' })
+  //     if (e.error) {
+  //       throw new UserRejectedRequestError(new Error(e.error))
+  //     }
+  //     if (!e.connected) {
+  //       throw new UserRejectedRequestError(new Error('Wallet connection rejected'))
+  //     }
+  //   }
+
+  //   const account = await this.getAccount()
+
+  //   return {
+  //     account,
+  //     chain: {
+  //       id: this.provider.getChainId(),
+  //       unsupported: this.isChainUnsupported(this.provider.getChainId()),
+  //     },
+  //   }
+  // }
+
+  // async getWalletClient({ chainId }: { chainId?: number } = {}): Promise<any> {
+  //   const chain = this.chains.find((x) => x.id === chainId)
+
+  //   return createWalletClient({
+  //     chain,
+  //     account: await this.getAccount(),
+  //     transport: custom(this.provider),
+  //   })
+  // }
+
+  // protected onChainChanged(chain: string | number): void {
+  //   this.provider.setDefaultChainId(normalizeChainId(chain))
+  // }
+
+  // async switchChain(chainId: number): Promise<Chain> {
+  //   if (this.isChainUnsupported(chainId)) {
+  //     throw new Error('Unsupported chain')
+  //   }
+
+  //   this.provider.setDefaultChainId(chainId)
+  //   return this.chains.find((x) => x.id === chainId) as Chain
+  // }
+
+  // async disconnect() {
+  //   this.provider.disconnect()
+  // }
+
+  // getAccount() {
+  //   return this.provider.getSigner().getAddress() as Promise<`0x${string}`>
+  // }
+
+  // async getChainId() {
+  //   return this.provider.getChainId()
+  // }
+
+  // async getProvider() {
+  //   return this.provider
+  // }
+
+  // async getSigner() {
+  //   return this.provider.getSigner()
+  // }
+
+  // async isAuthorized() {
+  //   try {
+  //     const account = await this.getAccount()
+  //     return !!account
+  //   } catch {
+  //     return false
+  //   }
+  // }
+
+  // protected onAccountsChanged = (accounts: string[]) => {
+  //   return { account: accounts[0] }
+  // }
+
+  // protected onDisconnect = () => {
+  //   // @ts-ignore-next-line
+  //   this?.emit('disconnect')
+  // }
+
+  // isChainUnsupported(chainId: number): boolean {
+  //   return this.provider.networks.findIndex((x) => x.chainId === chainId) === -1
+  // }
 
 function normalizeChainId(chainId: string | number | bigint | { chainId: string }) {
   if (typeof chainId === 'object') return normalizeChainId(chainId.chainId)
